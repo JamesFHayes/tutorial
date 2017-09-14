e69e32cde9ccca267fe358b87f018f86c8f3a1ba
JamesFHayes <31931737+JamesFHayes@users.noreply.github.com>
2017-09-14 15:58:41 +0100

    no empty comment

--- src/power.py
+++ src/power.py
@@ -1,3 +1,4 @@
+"""This is a Script that calculates one number to the power of another."""
 def to the power(x, y = 2):
 	result = x
 	for i in range(0, y):
@@ -5,5 +6,5 @@ def to the power(x, y = 2):
 	return result
 	
 x = 9
-y = 6
+y = 8
 print("{0} to the power of {2} is: ".format(x, y, to the power(x, y)))
