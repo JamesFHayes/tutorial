b84f83f83ead21dbaa888d7f9872f6e3d447be46
James Hayes <james.hayes1@mycit.ie>
2017-09-16 13:53:16 +0100

    full solution that fixes #3

--- src/power.py
+++ src/power.py
@@ -1,30 +1,17 @@
 """This is a Script that calculates one number to the power of another."""
 
-def to the power(x, y = 2):
-	result = x
-	for i in range(0, y):
-		result = result * x
-	return result
-	
-x = 6 
-
-"""trying this out"""
-
-return x ** y	
-	
-	#result = x
-	#for i in range(0, y):
-	#	result = result * x
-	#return result
+import sys
 import math
+
+def to_the_power(x, y = 2):
+    """ trying this out
+    """
+    return x ** y
+
 def square_root(x):
-		return math.sqrt(x)
-x = 5 #
+    return math.sqrt(x)
 
-
-y = 8 #
-
-
-print("{0} to the power of {2} is: ".format(x, y, to the power(x, y)))
+x = int(sys.argv[1])
+y = int(sys.argv[2]) 
+print("{0} to the power of {1} is {2}".format(x, y, to_the_power(x, y)))
 print("The square root of {0} is {1}".format(x, square_root(x)))
-print("The End")
